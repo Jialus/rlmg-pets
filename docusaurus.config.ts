8e819f2e89286427b88b063a6e43c12fecff4e5c
Jialu Sun <jialusun@Jialus-MacBook-Pro.local>
2025-02-04 00:54:15 -0500

    update config to make sure url is correct

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/rlmg-logo.ico',
 
   // Set the production url of your site here
-  url: 'https://rlmg-cute.com',
+  url: 'https://jialus.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/rlmg-pets/',
