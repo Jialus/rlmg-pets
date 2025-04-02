88b43e57ad33fdfe481989aa7dabc11afcdd3eda
Jialu Sun <jialusun@Jialus-MacBook-Pro.local>
2025-04-02 09:10:05 -0400

    change back to riginal base url for github pages depoly

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://jialus.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rlmg-wiki/',
+  baseUrl: '/rlmg-pets/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
